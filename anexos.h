da11ede80e064b5dc3076f89d325e364cd6d7eb3
VatoDev <73704763+ElVatoEste@users.noreply.github.com>
2022-05-22 17:41:13 -0600

    Add files via upload

---
+++ anexos.h
@@ -0,0 +1,34 @@
+#ifndef ANEXOS_H
+#define ANEXOS_H
+
+#include <vector>
+#include <iostream>
+#include <cctype>
+#include <bits/stdc++.h>
+#include <conio.h>
+#include <algorithm>
+#include <string>
+#include <locale.h> 
+#include <sstream>
+#include <fstream>
+
+/* validation
+bool doesItHaveSpecialCharacters(std::string &nom);
+bool isItAnewUser(std::string old, std::string nuevo);
+bool isItAnEmail(std::string &email);
+bool isItACreditCard(std::string &card);
+bool creditCardExpiryLength(std::string card);
+bool creditCardExpiryLengthM(std::string card);
+bool isItcreditCardCVV(std::string &card);
+
+//input & data Transformation
+std::string inputString();
+std::string dateTransformation(std::string &expiryM, std::string &expiryY);
+
+//menus
+void displayMenu();
+void confirmationMenu(std::string nombre, std::string apellido, std::string email, std::string creditCard,std::string date, std:: string CVV, std::string password);
+*/
+
+
+#endif
